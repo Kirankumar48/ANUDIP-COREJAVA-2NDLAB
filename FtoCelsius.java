db63e10b83b1a80c01253e6737faf47ddc09d197
Adanya Kiran <adanyakiran9701@gmail.com>
2024-07-18 21:39:42 +0530

    Add files via upload

---
+++ FtoCelsius.java
@@ -0,0 +1,13 @@
+package kiran.java;
+
+public class FtoCelsius {
+	public static void main(String args[])
+	{
+		double fahrenheit =35;
+		double celsius = ((5 * (fahrenheit - 32.0)) / 9.0);
+		System.out.println(celsius);
+
+		
+	}
+
+}
