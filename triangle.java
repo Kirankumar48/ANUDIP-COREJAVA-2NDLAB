fcea0e9540f5ea93336744975ff057b9c3735f0f
Adanya Kiran <adanyakiran9701@gmail.com>
2024-07-18 21:40:51 +0530

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,28 @@
+package kiran.java;
+
+public class triangle {
+	
+	public static void main(String args[])
+	{
+		int angle1 = 40;
+		int angle2 = 50;
+		int angle3 = 90;
+		
+		int triangle = angle1+angle2+angle3;
+		
+		if(triangle==180)
+		{
+			System.out.println("It is a triangle");
+			
+		}
+		else
+		{
+			System.out.println("It is not a triangle");
+			
+		}
+			
+		
+		
+	}
+
+}
