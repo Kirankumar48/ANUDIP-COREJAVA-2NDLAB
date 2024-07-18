634abec87fd4689ea2450f6c4456bcdb32b5d797
Adanya Kiran <adanyakiran9701@gmail.com>
2024-07-18 21:40:11 +0530

    Add files via upload

---
+++ simpleInterest.java
@@ -0,0 +1,17 @@
+package kiran.java;
+
+public class simpleInterest {
+	
+	public static void main(String args[])
+	{
+		float p = 20000;
+		int t = 12;
+		int r = 3;
+		
+		float interest = (p*t*r)/100;
+		
+		System.out.println(interest);
+		
+	}
+
+}
